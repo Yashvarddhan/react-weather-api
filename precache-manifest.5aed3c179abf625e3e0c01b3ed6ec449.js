4aa5c4efd073876caa3c5aa34de79a26cdbd4eef
Yashvarddhan <yashvarddhansingh12@gmail.com>
2023-07-25 14:07:33 +0530

    Updates

---
+++ precache-manifest.5aed3c179abf625e3e0c01b3ed6ec449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "33857f642ee87f37291e",
+    "url": "/react-weather-api/static/css/main.8a4f405f.chunk.css"
+  },
+  {
+    "revision": "33857f642ee87f37291e",
+    "url": "/react-weather-api/static/js/main.d72c83f0.chunk.js"
+  },
+  {
+    "revision": "c8626e58a117d690e201",
+    "url": "/react-weather-api/static/js/runtime~main.929e52a8.js"
+  },
+  {
+    "revision": "0ecd6e66d1cd2258b603",
+    "url": "/react-weather-api/static/js/2.c2d3146c.chunk.js"
+  },
+  {
+    "revision": "a88019978856cdd16b1fbdd8974f87b0",
+    "url": "/react-weather-api/static/media/sunny.a8801997.jpg"
+  },
+  {
+    "revision": "bdda301c4a3c24a3a40414260df9232e",
+    "url": "/react-weather-api/static/media/winter.bdda301c.jpg"
+  },
+  {
+    "revision": "78981660655294d1b443edce886d0540",
+    "url": "/react-weather-api/index.html"
+  }
+];
